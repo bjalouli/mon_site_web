617d2ea8e6f3a74c71e6a44493c15d04433aea88
Bilel Jalouli <bilel.jlouli@insat.ucar.tn>
2023-09-05 12:29:50 +0200

    initial build using IDE

--- Debug/App/gpio-app/subdir.mk
+++ Debug/App/gpio-app/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -15,13 +15,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-App/gpio-app/%.o: ../App/gpio-app/%.c App/gpio-app/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+App/gpio-app/%.o App/gpio-app/%.su App/gpio-app/%.cyclo: ../App/gpio-app/%.c App/gpio-app/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-App-2f-gpio-2d-app
 
 clean-App-2f-gpio-2d-app:
-	-$(RM) ./App/gpio-app/app_main.d ./App/gpio-app/app_main.o
+	-$(RM) ./App/gpio-app/app_main.cyclo ./App/gpio-app/app_main.d ./App/gpio-app/app_main.o ./App/gpio-app/app_main.su
 
 .PHONY: clean-App-2f-gpio-2d-app
 
